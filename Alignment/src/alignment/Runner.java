489e25aa2bab7dfe25b6f6ce1feaf389a06130e7
Ron Fechtner <ronfechtner@gmail.com>
2016-02-23 19:44:37 +0100

    Runner done

--- Alignment/src/alignment/Runner.java
+++ Alignment/src/alignment/Runner.java
@@ -9,13 +9,29 @@ public class Runner {
 		
 		ScoringMatrix sm = new ScoringMatrix(p.getM());
 		GapFunction gf = new GapFunction(p.getGo(), p.getGe());
-		
 		ArrayList<SequencePair> sequencePairs = SequenceParser.parseSeq(p.getSeqlib(), p.getPairs());
 		
-		//for ( SequencePair sp : sequencePairs ){
-		//	System.out.println(sp.toString());
-		//}
+		for ( SequencePair sp : sequencePairs ){
+			Alignment al = null;
+			if(p.isNw()) {
+				switch (p.getMode()){
+					case local: al = new Local(sp, gf, sm); break;
+					case global: al = new Global(sp, gf, sm); break;
+					case freeshift: al = new Freeshift(sp, gf, sm); break;
+				}
+			} else {
+				switch (p.getMode()){
+					case local: al = new GotohLocal(sp, gf, sm); break;
+					case global: al = new GotohGlobal(sp, gf, sm); break;
+					case freeshift: al = new GotohFreeshift(sp, gf, sm); break;
+				}
+			}
+			
+			al.make();
+			
+			//System.out.println("Alignment für " + sp.toString());
+			//System.out.println("\t Alignment: " + al.getFinalAlignment().toString());
+			//System.out.println("\t Score: " + al.getFinalScore());
+		}
 	}
-	
-	
 }
